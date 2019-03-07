ec73942226a8ef73722cfd04bb344661afe7b138
vu-minh <vuvietminh@unamur.be>
2019-03-07 12:28:18 +0100

    dash_app extrace logic to get data to data_filter module

---
+++ code/dash_app/data_filter.py
@@ -0,0 +1,20 @@
+"""Data selection and filtering, process the logics,
+    find best perp according to user constraints.
+"""
+
+import os
+import joblib
+import numpy as np
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+embedding_dir = f"{dir_path}/embeddings"
+DEV = True
+
+
+def get_embedding(dataset_name, perp):
+    in_name = f"{embedding_dir}/{dataset_name}/{int(perp)}.z"
+    tsne = joblib.load(in_name)
+    print(type(tsne))
+    Z = tsne.embedding_
+    return Z[:200] if DEV else Z
