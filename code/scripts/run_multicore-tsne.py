1070e1e5e9042481170b7ad1932c8f8add49d4a5
vu-minh <vuvietminh@unamur.be>
2019-03-07 16:00:19 +0100

    script to run multicore tsne with a dataset

--- code/scripts/run_multicore-tsne.py
+++ code/scripts/run_multicore-tsne.py
@@ -14,50 +14,56 @@ USE_MULTICORE = True
 fixed_seed = 2019
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-dataset_name = 'FASHION2000'
 data_dir = f"{dir_path}/data"
-embedding_dir = f"{dir_path}/embeddings/{dataset_name}"
-if not os.path.exists(embedding_dir):
-    os.mkdir(embedding_dir)
-
-
-perp = 40
 dataset.set_data_home(data_dir)
-_, X, y = dataset.load_dataset(dataset_name)
 
-start_time = time()
-if USE_MULTICORE:
-    tsne = MulticoreTSNE(n_jobs=4, random_state=fixed_seed, perplexity=perp)
-else:
-    tsne = TSNE(random_state=fixed_seed, perplexity=perp)
-tsne.fit_transform(X)
-running_time = time() - start_time
-print(f"Running time: {running_time}")
 
-result = dict(
-    running_time=running_time,
-    embedding=tsne.embedding_,
-    kl_divergence=tsne.kl_divergence_,
-    n_jobs=tsne.n_jobs if USE_MULTICORE else 0,
-    n_iter=tsne.n_iter_,
-    learning_rate=tsne.learning_rate,
-    random_state=tsne.random_state,
-)
+def run_dataset(dataset_name):
+    _, X, y = dataset.load_dataset(dataset_name)
+    embedding_dir = f"{dir_path}/embeddings/{dataset_name}"
+    if not os.path.exists(embedding_dir):
+        os.mkdir(embedding_dir)
 
-for k, v in result.items():
-    if k != 'embedding':
-        print(k, v)
+    for perp in range(1, X.shape[0] // 3):
+        start_time = time()
+        if USE_MULTICORE:
+            tsne = MulticoreTSNE(random_state=fixed_seed, perplexity=perp,
+                                 n_jobs=4)
+        else:
+            tsne = TSNE(random_state=fixed_seed, perplexity=perp)
+        tsne.fit_transform(X)
+        running_time = time() - start_time
+        print(f"{dataset_name}, {perp}, time: {running_time}s")
 
-out_name = f"{embedding_dir}/{perp}.z"
-joblib.dump(value=result, filename=out_name, compress=True)
+        result = dict(
+            running_time=running_time,
+            embedding=tsne.embedding_,
+            kl_divergence=tsne.kl_divergence_,
+            n_jobs=tsne.n_jobs if USE_MULTICORE else 1,
+            n_iter=tsne.n_iter_,
+            learning_rate=tsne.learning_rate,
+            random_state=tsne.random_state,
+        )
+        joblib.dump(value=result, filename=f"{embedding_dir}/{perp}.z")
 
-# test load data
-print("Test saved data from ", out_name)
-loaded = joblib.load(filename=out_name)
-for k, v in loaded.items():
-    if k == 'embedding':
-        Z = loaded[k]
-        plt.scatter(Z[:, 0], Z[:, 1], c=y)
-        plt.savefig(f"test_{'multicore' if USE_MULTICORE else 'sklearn'}.png")
-    else:
-        print(k, v)
+
+def test_load_data(dataset_name, perp=30):
+    _, _, y = dataset.load_dataset(dataset_name)
+    embedding_dir = f"{dir_path}/embeddings/{dataset_name}"
+    out_name = f"{embedding_dir}/{perp}.z"
+    print("Test saved data from ", out_name)
+
+    loaded = joblib.load(filename=out_name)
+    for k, v in loaded.items():
+        if k == 'embedding':
+            Z = loaded[k]
+            plt.scatter(Z[:, 0], Z[:, 1], c=y, alpha=0.3)
+            plt.savefig(f"test_{'multicore' if USE_MULTICORE else 'sk'}.png")
+        else:
+            print(k, v)
+
+
+if __name__ == '__main__':
+    dataset_name = 'FASHION200'
+    run_dataset(dataset_name)
+    # test_load_data(dataset_name, perp=20)
