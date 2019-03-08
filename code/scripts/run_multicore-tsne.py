58e2fbf950fbb49e0856f326f247b51f05b76c71
vu-minh <vuvietminh@unamur.be>
2019-03-08 08:28:21 +0100

    Remove step pre-calculate Q

--- code/scripts/run_multicore-tsne.py
+++ code/scripts/run_multicore-tsne.py
@@ -3,39 +3,22 @@
 """
 import os
 import joblib
-import numpy as np
 from time import time
 from MulticoreTSNE import MulticoreTSNE
 from sklearn.manifold import TSNE
 from matplotlib import pyplot as plt
 from common.dataset import dataset
 import multiprocessing
-from scipy.spatial.distance import pdist, squareform
 
 
 DEV = False
 USE_MULTICORE = True
 fixed_seed = 2019
 n_cpu_using = int(0.75 * multiprocessing.cpu_count())
-MACHINE_EPSILON = np.finfo(np.double).eps
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_dir = f"{dir_path}/data"
 dataset.set_data_home(data_dir)
-
-
-def _compute_Q(X2d):
-    """ Matrix Q in t-sne, used to calculate the prob. that a point `j`
-    being neighbor of a point `i` (the value of Q[i,j])
-    Make sure to call squareform(Q) before using it.
-    """
-    degrees_of_freedom = 1
-    dist = pdist(X2d, "sqeuclidean")
-    dist /= degrees_of_freedom
-    dist += 1.
-    dist **= (degrees_of_freedom + 1.0) / -2.0
-    Q = np.maximum(dist / (2.0 * np.sum(dist)), MACHINE_EPSILON)
-    return Q
 
 
 def run_dataset(dataset_name):
@@ -59,7 +42,6 @@ def run_dataset(dataset_name):
             perplexity=perp,
             running_time=running_time,
             embedding=tsne.embedding_,
-            Q=_compute_Q(tsne.embedding_),
             kl_divergence=tsne.kl_divergence_,
             n_jobs=tsne.n_jobs if USE_MULTICORE else 1,
             n_iter=tsne.n_iter_,
@@ -81,9 +63,6 @@ def test_load_data(dataset_name, perp=30):
             Z = loaded[k]
             plt.scatter(Z[:, 0], Z[:, 1], c=y, alpha=0.3)
             plt.savefig(f"test_{'multicore' if USE_MULTICORE else 'sk'}.png")
-        elif k == 'Q':
-            Q = squareform(v)
-            print('Q matrix: ', Q.shape, Q.min(), Q.max())
         else:
             print(k, v)
 
