074f724d4d9819e60523f1326d7f091228e91703
vu-minh <vuvietminh@unamur.be>
2019-04-04 16:24:20 +0200

    Gen metaplots

--- code/scripts/run_plots.py
+++ code/scripts/run_plots.py
@@ -3,7 +3,8 @@ import joblib
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.manifold import TSNE
 from matplotlib import pyplot as plt
 from common.dataset import dataset
 from icommon import hyper_params
@@ -136,31 +137,73 @@ def plot_compare_kl_to_base(base_perp, key="kl_Qbase_Q", title="KL[Qbase || Q]")
     plt.savefig(f"{file_name}.png")  # , bbox_inches="tight", pad_inches=0)
 
 
+def plot_metamap(run_range, base_perp=None, earlystop=""):
+    print(f"Generate meta-plot for {dataset_name} with params: ", locals())
+
+    if base_perp is None:
+        embedding_dir = f"{dir_path}/normal/{dataset_name}"
+        in_name_prefix = ""
+        out_name_sufix = ""
+    else:
+        embedding_dir = f"{dir_path}/chain/{dataset_name}"
+        in_name_prefix = f"{base_perp}_to_"
+        out_name_sufix = f"_base{base_perp}"
+
+    _, X, _ = dataset.load_dataset(dataset_name)
+    run_range = run_range or range(3, X.shape[0] // 3)
+
+    # prepare all pre-calculated embeddings
+    all_Z = []
+    all_perps = []
+    all_losses = []
+    for perp in run_range:
+        in_name = f"{embedding_dir}/{in_name_prefix}{perp}{earlystop}.z"
+        data = joblib.load(in_name)
+        all_perps.append(perp)
+        all_Z.append(data["embedding"].ravel())
+        all_losses.append(data["kl_divergence"])
+
+    # using all_Z as features for meta-tSNE
+    # all_Z = StandardScaler().fit_transform(all_Z)
+    meta_Z = TSNE(perplexity=10).fit_transform(all_Z)
+
+    out_name = f"{dataset_name}{out_name_sufix}{earlystop}"
+    plt.figure(figsize=(6, 6))
+    plt.scatter(meta_Z[:, 0], meta_Z[:, 1], c=all_perps)
+    plt.title(out_name)
+    plt.colorbar()
+    plt.tight_layout()
+    plt.savefig(f"./plot_chain/metamap/{out_name}.png")
+
+
 if __name__ == "__main__":
     import argparse
 
     ap = argparse.ArgumentParser()
-    ap.add_argument("-d", "--dataset_name")
+    ap.add_argument("-d", "--dataset_name", default="FASHION200")
     ap.add_argument("-x", "--dev", action="store_true")
-    ap.add_argument("-p", "--test_perp")
-    args = vars(ap.parse_args())
+    ap.add_argument("-p", "--test_perp", default=30, type=int)
+    args = ap.parse_args()
 
-    dataset_name = args.get("dataset_name", "FASHION200")
-    test_perp = args.get("test_perp", 30)
-    DEV = args.get("dev", False)
+    dataset_name = args.dataset_name
+    test_perp = args.test_perp
+    DEV = args.dev
 
     run_range = [test_perp] if DEV else None
 
     for base_perp in [None] + (
         [40] if DEV else hyper_params[dataset_name]["base_perps"]
     ):  # base_perp = None to plot the embedding `normal`
-        plot_embeddings(run_range=run_range, base_perp=base_perp, force_rewrite=True)
+        # plot_embeddings(run_range=run_range, base_perp=base_perp, force_rewrite=True)
 
-        plot_extracted_info_by_key(
-            base_perp, key="running_time", title="Compare running time"
-        )
-        plot_extracted_info_by_key(
-            base_perp, key="n_iter", title="Compare number of running iterations"
-        )
+        # plot_extracted_info_by_key(
+        #    base_perp, key="running_time", title="Compare running time"
+        # )
+        # plot_extracted_info_by_key(
+        #    base_perp, key="n_iter", title="Compare number of running iterations"
+        # )
 
-        plot_compare_kl_to_base(base_perp)
+        # plot_compare_kl_to_base(base_perp)
+
+        for earlystop in ["", "_earlystop"]:
+            plot_metamap(run_range, base_perp, earlystop)
