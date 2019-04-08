fabac36474748080c95b4a3da08a790a411c2a37
vu-minh <vuvietminh@unamur.be>
2019-04-09 01:03:50 +0200

    fix bug indentation, add plot examples

--- code/scripts/plot_paper.py
+++ code/scripts/plot_paper.py
@@ -6,29 +6,12 @@ import argparse
 from sklearn.manifold import TSNE
 from matplotlib import pyplot as plt
 from common.dataset import dataset
+from common.plot.scatter import scatter_with_box, annotate_text, config_font_size
 from icommon import hyper_params
 from matplotlib.ticker import FuncFormatter
 
 # import pandas as pd
 # from sklearn.preprocessing import MinMaxScaler, StandardScaler
-
-
-def scatter_with_box(ax, all_pos, marker="s", color="blue"):
-    ax.scatter(
-        all_pos[:, 0],
-        all_pos[:, 1],
-        marker=marker,
-        s=256,
-        facecolor="none",
-        edgecolor=color,
-        linewidth=2.0,
-    )
-
-
-def annotate_text(ax, text, pos, text_color="blue", offset=(-10, 10)):
-    ax.annotate(
-        s=str(text), xy=pos, xytext=offset, textcoords="offset points", color=text_color
-    )
 
 
 def plot_metamap(
@@ -45,69 +28,71 @@ def plot_metamap(
         in_name_prefix = f"{base_perp}_to_"
         out_name_sufix = f"_base{base_perp}"
 
-        # prepare all pre-calculated embeddings
-        all_Z = []
-        all_losses = []
-        highlight_idx = {}
-        for idx, perp in enumerate(all_perps):
-            in_name = f"{embedding_dir}/{in_name_prefix}{perp}{earlystop}.z"
-            data = joblib.load(in_name)
-            all_Z.append(data["embedding"].ravel())
-            all_losses.append(data["kl_divergence"])
-            if perp in highlight_selected_perps:
-                highlight_idx[idx] = perp
+    # prepare all pre-calculated embeddings
+    all_Z = []
+    all_losses = []
+    highlight_idx = {}
+
+    for idx, perp in enumerate(all_perps):
+        in_name = f"{embedding_dir}/{in_name_prefix}{perp}{earlystop}.z"
+        data = joblib.load(in_name)
+        all_Z.append(data["embedding"].ravel())
+        all_losses.append(data["kl_divergence"])
+        if perp in highlight_selected_perps:
+            highlight_idx[idx] = perp
 
     if len(all_Z) < 1:
         raise ValueError("Not enough embeddings to make metaplot")
 
-        # all_Z = StandardScaler().fit_transform(all_Z)
-        meta_Z = TSNE(perplexity=10, random_state=2019).fit_transform(all_Z)
+    # all_Z = StandardScaler().fit_transform(all_Z)
+    meta_Z = TSNE(perplexity=10, random_state=2019).fit_transform(all_Z)
 
+    ax.grid(False)
     sct = ax.scatter(meta_Z[:, 0], meta_Z[:, 1], c=all_perps, cmap="inferno")
     ax.set_title(f"{dataset_name}{out_name_sufix}")
+
+    # show colorbar beside the metaplot
+    cbar = plt.colorbar(sct, aspect=40, pad=0.04)
+    cbar.ax.set_title("Perplexity")
 
     # highlight selected perplexities
     if len(highlight_idx) > 0:
         highlight_pos = meta_Z[list(highlight_idx.keys())]
         scatter_with_box(ax, all_pos=highlight_pos)
-
+        # add annotation text above the box
         for idx, selected_perp in enumerate(highlight_idx.values()):
             annotate_text(ax, text=selected_perp, pos=highlight_pos[idx])
 
-            # highlight base perplexity
-            base_perp_idx = all_perps.index(base_perp)
-            scatter_with_box(
-                ax,
-                all_pos=np.array([meta_Z[base_perp_idx, :]]),
-                marker="h",
-                color="red",
-            )
-            annotate_text(
-                ax,
-                text=base_perp,
-                pos=meta_Z[base_perp_idx],
-                offset=(-18, -24),
-                text_color="red",
-            )
+    # highlight base perplexity
+    base_perp_idx = all_perps.index(base_perp)
+    scatter_with_box(
+        ax, all_pos=np.array([meta_Z[base_perp_idx, :]]), marker="h", color="red"
+    )
+    annotate_text(
+        ax, base_perp, pos=meta_Z[base_perp_idx], offset=(-18, -24), text_color="red"
+    )
 
-            # show colorbar beside the metaplot
-            cbar = plt.colorbar(sct, aspect=40, pad=0.04)
-            cbar.ax.set_title("Perplexity")
-            plt.tight_layout()
+    plt.tight_layout()
 
 
-def plot_an_embedding(ax, perp, base_perp=None, labels=None):
+def plot_an_embedding(ax, perp, base_perp=None, labels=None, disable_ticks=False):
     if base_perp is None:
         embedding_dir = f"{dir_path}/normal/{dataset_name}"
         in_name_prefix = ""
     else:
         embedding_dir = f"{dir_path}/chain/{dataset_name}"
         in_name_prefix = f"{base_perp}_to_"
-        in_name = f"{embedding_dir}/{in_name_prefix}{perp}{earlystop}.z"
-        data = joblib.load(in_name)
-        Z = data["embedding"]
-        ax.scatter(Z[:, 0], Z[:, 1], c=labels, alpha=0.5, cmap="jet")
-        ax.set_title(f"perplexity = {perp}")
+
+    in_name = f"{embedding_dir}/{in_name_prefix}{perp}{earlystop}.z"
+    data = joblib.load(in_name)
+    Z = data["embedding"]
+    ax.scatter(Z[:, 0], Z[:, 1], c=labels, alpha=0.5, cmap="jet")
+    ax.grid(False)
+    ax.set_title(f"perplexity = {perp}")
+
+    if disable_ticks:
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
 
 
 def plot_metamap_with_some_perps(
@@ -122,6 +107,7 @@ def plot_metamap_with_some_perps(
     _, X, labels = dataset.load_dataset(dataset_name)
     all_perps = range(3, X.shape[0] // 3)
 
+    # plot metaplot
     if with_metamap:
         ax0 = plt.subplot2grid((n_rows, n_cols), (0, 3), rowspan=2, colspan=2)
         ax0.set_xticklabels([])
@@ -132,22 +118,17 @@ def plot_metamap_with_some_perps(
             base_perp=base_perp,
             highlight_selected_perps=selected_perps,
         )
-        for i, perp in enumerate(selected_perps):
-            row_i, col_i = i // 3, i % 3
-            ax_i = plt.subplot2grid((n_rows, n_cols), (row_i, col_i))
-            ax_i.set_xticklabels([])
-            ax_i.set_yticklabels([])
-            plot_an_embedding(ax=ax_i, perp=perp, base_perp=base_perp, labels=labels)
+
+    # plot embeddings corresponding to the selected perplexities
+    for i, perp in enumerate(selected_perps):
+        row_i, col_i = i // 3, i % 3
+        ax_i = plt.subplot2grid((n_rows, n_cols), (row_i, col_i))
+        plot_an_embedding(
+            ax=ax_i, perp=perp, base_perp=base_perp, labels=labels, disable_ticks=True
+        )
 
     plt.tight_layout()
     plt.savefig(f"{out_name}.{OUTPUT_EXT}")
-
-
-def log_e_format(x, pos):
-    """The two args are the value and tick position.
-	Label ticks with the product of the exponentiation
-	"""
-    return int(x)
 
 
 def plot_running_time(
@@ -163,6 +144,7 @@ def plot_running_time(
     embedding_dir_normal = f"{dir_path}/normal/{dataset_name}"
     embedding_dir_chain = f"{dir_path}/chain/{dataset_name}"
 
+    # prepare data: read csv for all perplexities of one dataset
     for perp in all_perps:
         in_name_normal = f"{embedding_dir_normal}/{perp}{earlystop}.z"
         data_normal = joblib.load(in_name_normal)
@@ -175,6 +157,7 @@ def plot_running_time(
         running_time_chains.append(running_time_chain)
         n_iter_chains.append(data_chain["n_iter"])
 
+    # plot running time for one dataset
     if perp_in_log_scale:
         ax.semilogx(
             all_perps, running_time_normals, lw=2.0, label="tSNE normal", basex=np.e
@@ -183,7 +166,7 @@ def plot_running_time(
             all_perps, running_time_chains, lw=2.0, label="chain-tSNE", basex=np.e
         )
         ax.set_xscale("log", basex=np.e)
-        ax.xaxis.set_major_formatter(FuncFormatter(log_e_format))
+        ax.xaxis.set_major_formatter(FuncFormatter(lambda value, pos: int(value)))
         ax.set_xtitle("perplexity in log-scale")
     else:
         ax.plot(all_perps, running_time_normals, lw=2.0, label="tSNE normal")
@@ -198,7 +181,7 @@ def plot_running_time(
 
 def plot_running_time_all_datasets(dataset_names, base_perp, out_name):
     (n_rows, n_cols) = (1, len(dataset_names))
-    plt.figure(figsize=(9 * n_cols, 6 * n_rows))
+    plt.figure(figsize=(8 * n_cols, 6 * n_rows))
 
     for i, dataset_name in enumerate(dataset_names):
         ax_i = plt.subplot2grid((n_rows, n_cols), (0, i))
@@ -208,13 +191,12 @@ def plot_running_time_all_datasets(dataset_names, base_perp, out_name):
     plt.savefig(f"{out_name}.{OUTPUT_EXT}")
 
 
-def plot_example(dataset_name, selected_perp, base_perp=30, out_name=""):
-    pass
-
-
 if __name__ == "__main__":
-    plt.rcParams.update({"font.size": 22})
+    # plt.rcParams.update({"font.size": 22})
+    config_font_size(min_size=16)
+
     OUTPUT_EXT = "png"  # "pdf" for paper
+    FIG_DIR = "../../figures"
     dir_path = os.path.dirname(os.path.realpath(__file__))
     data_dir = f"{dir_path}/data"
     dataset.set_data_home(data_dir)
@@ -234,7 +216,7 @@ if __name__ == "__main__":
     earlystop = "_earlystop" if args.earlystop else ""
 
     def _plot_metamap():
-        out_name = f"../../figures/metaplot_{dataset_name}_base{base_perp}"
+        out_name = f"{FIG_DIR}/metaplot_{dataset_name}_base{base_perp}"
         plot_metamap_with_some_perps(
             selected_perps=hyper_params[dataset_name]
             .get("selected_perps", {})
@@ -245,10 +227,26 @@ if __name__ == "__main__":
         )
 
     def _plot_running_time():
-        out_name = f"../../figures/runningtime_base{base_perp}"
+        out_name = f"{FIG_DIR}/runningtime_base{base_perp}"
         dataset_names = ["BREAST_CANCER", "COIL20", "FASHION200", "COUNTRY2014"]
         plot_running_time_all_datasets(dataset_names, base_perp, out_name=out_name)
 
-        # _plot_metamap()
+    def _plot_examples():
+        from itertools import product
 
+        _, _, labels = dataset.load_dataset(dataset_name)
+        base_perps = [None, 30]
+        selected_perps = [5, 40]
+
+        for base_perp, selected_perp in product(base_perps, selected_perps):
+            name_prefix = "" if base_perp is None else f"base{base_perp}"
+            out_name = f"{FIG_DIR}/eg_{dataset_name}{name_prefix}_perp{selected_perp}"
+
+            _, ax = plt.subplots(1, 1, figsize=(6, 6))
+            plot_an_embedding(ax, selected_perp, base_perp, labels=labels)
+            plt.tight_layout()
+            plt.savefig(f"{out_name}.{OUTPUT_EXT}")
+
+    _plot_metamap()
     _plot_running_time()
+    _plot_examples()
