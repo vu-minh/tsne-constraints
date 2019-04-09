259eb2a93ebe568e31da4a45a0574c3fcd1f9c6e
vu-minh-dev <vuvietminh@unamur.be>
2019-04-09 10:24:37 +0000

    Update plot_paper for ploting n_iter, format run_metrics

--- code/scripts/run_metrics.py
+++ code/scripts/run_metrics.py
@@ -20,8 +20,11 @@ if not os.path.exists(metric_dir):
 
 def get_list_embeddings(dataset_name):
     target_dir = os.path.join(embedding_dir, dataset_name)
-    return [os.path.join(embedding_dir, dataset_name, f)
-            for f in os.listdir(target_dir) if f.endswith('.z')]
+    return [
+        os.path.join(embedding_dir, dataset_name, f)
+        for f in os.listdir(target_dir)
+        if f.endswith(".z")
+    ]
 
 
 def calculate_metrics(dataset_name, metric_names):
@@ -30,7 +33,7 @@ def calculate_metrics(dataset_name, metric_names):
 
     results = []
     for in_file_name in get_list_embeddings(dataset_name):
-        perp = int(in_file_name.split('/')[-1][:-2])
+        perp = int(in_file_name.split("/")[-1][:-2])
         data = joblib.load(in_file_name)
 
         # Recall: data = dict(
@@ -43,43 +46,36 @@ def calculate_metrics(dataset_name, metric_names):
         #     random_state=tsne.random_state,
         # )
 
-        data['perplexity'] = perp
-        data['bic'] = 2 * data['kl_divergence'] + np.log(N) * perp / N
+        data["perplexity"] = perp
+        data["bic"] = 2 * data["kl_divergence"] + np.log(N) * perp / N
 
-        drMetric = DRMetric(X, data['embedding'])
+        drMetric = DRMetric(X, data["embedding"])
         for metric_name in metric_names:
             metric_method = getattr(drMetric, metric_name)
             data[metric_name] = metric_method()
         # do not need the embedding field
-        del data['embedding']
+        del data["embedding"]
         results.append(data)
 
     write_metric_results(dataset_name, results)
 
 
 def write_metric_results(dataset_name, results):
-    header = ','.join(results[0].keys())
-    body = [','.join(map(str, row.values())) for row in results]
-    with open(f"{metric_dir}/{dataset_name}.csv", 'w') as csv_file:
+    header = ",".join(results[0].keys())
+    body = [",".join(map(str, row.values())) for row in results]
+    with open(f"{metric_dir}/{dataset_name}.csv", "w") as csv_file:
         csv_file.write(header)
-        csv_file.write('\n')
-        csv_file.write('\n'.join(body))
+        csv_file.write("\n")
+        csv_file.write("\n".join(body))
 
 
 def test_metric_results(dataset_name):
-    df = pd.read_csv(f"{metric_dir}/{dataset_name}.csv",
-                     index_col='perplexity')
+    df = pd.read_csv(f"{metric_dir}/{dataset_name}.csv", index_col="perplexity")
     print(df)
 
 
-if __name__ == '__main__':
-    dataset_name = 'FASHION500'
-    metric_names = [
-        'auc_rnx',
-        'pearsonr',
-        'mds_isotonic',
-        'cca_stress',
-        'sammon_nlm'
-    ]
+if __name__ == "__main__":
+    dataset_name = "COIL20"
+    metric_names = ["auc_rnx", "pearsonr", "mds_isotonic", "cca_stress", "sammon_nlm"]
     calculate_metrics(dataset_name, metric_names)
     test_metric_results(dataset_name)
