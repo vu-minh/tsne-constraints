32566b2d1d162e148a4dec82c29fe3dcddb86f27
vu-minh-dev <vuvietminh@unamur.be>
2019-04-11 14:29:34 +0000

    Annote workflow in sh script

--- code/scripts/run.sh
+++ code/scripts/run.sh
@@ -5,15 +5,28 @@
 
 DATASET_NAME="COIL20"
 
+# step1: run tsne normal
 python run_multicore-tsne.py -d $DATASET_NAME
 
+
+# step2: run tsne in chain
+# it will use some pre-calculated embeddings (corresponding to `base_perps`) in step1
 python run_chain_perp.py -d $DATASET_NAME
 
-# run compare only for copying file
+
+# step3: run kl compare (KL[Qbase||Q])
+# but now use this step only for copying file
 python run_compare2.py -d $DATASET_NAME
 
+
+# step4a: run metric for tSNE normal, so do not specify base_perp
 python run_metrics.py -d $DATASET_NAME
 
-# python run_plots.py -d $DATASET_NAME
+# step4b: run metric for tSNE chain with list base_perps defined in the config
+python run_metrics.py -d $DATASET_NAME -bp 0
 
+
+# other steps: create scatter plots and create html page to show them all
+
+# python run_plots.py -d $DATASET_NAME
 # python render_view2.py -d $DATASET_NAME
